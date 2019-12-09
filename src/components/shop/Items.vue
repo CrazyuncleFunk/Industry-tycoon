a09f2d5066b9d40b646f6f6428a96c8fd3036d2d
Erin Dunn <erindunn@live.co.uk>
2019-12-09 05:18:15 +0000

    first

---
+++ src/components/shop/Items.vue
@@ -0,0 +1,16 @@
+<template>
+       <b-card :title="thing.name" align="center">
+      <b-card-text align="center" >
+          Daily Revenue: {{thing.revenue}}
+      </b-card-text>
+      <b-card-footer align="right" >
+        
+        Cost:{{thing.cost}} <b-button href="#" variant="success" >Buy</b-button>
+      </b-card-footer>
+    </b-card>
+</template> 
+<script>
+export default {
+     props:['thing'],
+}
+</script> 
---
+++ src/components/shop/Shop.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <b-card-group deck>
+            <app-items v-for="(thing,id) in things" :key="id" :thing="thing"></app-items>
+        </b-card-group>
+    </div>
+</template>
+<script>
+import Items from './Items'
+export default {
+    data(){
+        return{
+            things: [
+                {name: 'Clothing Factory', cost: 10000, upgrade:0,revenue:100},
+                {name: 'Sporting Goods Factory', cost: 10000, upgrade:0,revenue:100},
+                {name: 'Electronic Goods Factory', cost: 10000, upgrade:0,revenue:100}
+            ]
+        }
+    },
+    components:{
+        appItems: Items
+    }
+}
+</script>script  
